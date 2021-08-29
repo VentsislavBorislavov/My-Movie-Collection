d7e23c43da49aed5b978d8a337f5de980b308a10
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-08-30 02:39:47 +0300

    added image field to Movie model

--- prisma/migrations/20210829230511_movie_migration/migration.sql
+++ prisma/migrations/20210829230511_movie_migration/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE `Movie` (
     `note` VARCHAR(191) NOT NULL DEFAULT '',
     `rating` INTEGER NOT NULL DEFAULT 0,
     `isFavorite` BOOLEAN NOT NULL DEFAULT false,
-    `image` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NOT NULL DEFAULT '',
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
