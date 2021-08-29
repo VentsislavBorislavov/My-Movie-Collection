14db228596f3505656c976b0651d4fd33cb771fe
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-08-30 02:36:04 +0300

    people can add and edit notes

---
+++ pages/api/movie/[id]/note.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+const prisma = new PrismaClient()
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const filmId = req.query.id;
+    if (isNaN(filmId)) {
+        res.status(400).json({ message: "Invalid data" })
+        return
+    }
+
+    let note = await prisma.movie.findFirst({ where: { id: +filmId } })
+
+    if (req.method === "POST") {
+        try {
+            if (req.body !== "string") {
+                res.status(400).json({ message: "Invalid data" })
+            }
+            if (note) {
+                note = await prisma.movie.update({ where: { id: +filmId }, data: { note: req.body } })
+            } else {
+                note = await prisma.movie.create({ data: { id: +filmId, note: req.body } })
+            }
+        } catch (error) {
+            res.status(500).json(JSON.stringify(error))
+        }
+    }
+    note ?
+        res.json(note) : res.status(404).json({ message: "Note not found" });
+
+}
