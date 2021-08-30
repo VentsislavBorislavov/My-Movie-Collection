07039ea6ba9207a1681a51b680e4f746cb7b1669
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-08-30 03:26:03 +0300

    created function to check if id is valid

--- pages/api/movie/[id]/index.ts
+++ pages/api/movie/[id]/index.ts
@@ -2,13 +2,16 @@ import { PrismaClient } from "@prisma/client";
 import { NextApiResponse, NextApiRequest } from "next";
 const prisma = new PrismaClient()
 
-export default async ({query: {id: filmId}}: NextApiRequest, res: NextApiResponse) =>{
-    if(isNaN(filmId)){
-        res.status(400).json({message: "The film id must be of type number!"})
+export default async ({ query: { id: filmId } }: NextApiRequest, res: NextApiResponse) => {
+    if (isValidId(filmId)) {
+        res.status(400).json({ message: "The film id must be of type number!" })
         return
     }
 
-    const movie = await  prisma.movie.findFirst({where: {id: +filmId}})
+    const movie = await prisma.movie.findFirst({ where: { id: +filmId } })
     res.json(movie);
 }
 
+export const isValidId = (id: any) => {
+    return isNaN(id) && id % 1 === 0;
+}
