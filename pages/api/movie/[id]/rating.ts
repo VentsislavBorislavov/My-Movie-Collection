07b876397c45c7319cd7b732262ae978d5ba90b3
Vencislav Borislavov <64546764+VentsislavBorislavov@users.noreply.github.com>
2021-08-30 03:41:01 +0300

    Rating movies api

---
+++ pages/api/movie/[id]/rating.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+import { isValidId } from ".";
+
+
+
+const prisma = new PrismaClient()
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const filmId = req.query.id;
+    const rating = +req.body
+    
+    if (!isValidId(filmId) || rating > 5 || rating < 0) {
+        res.status(400).json({ message: "Invalid data" })
+        return
+    }
+    
+    let movie = await prisma.movie.findFirst({ where: { id: +filmId } })
+
+    if (req.method === "POST") {
+        try {
+            if (movie) {
+                movie = await prisma.movie.update({ where: { id: +filmId }, data: { rating: +req.body } })
+            } else {
+                movie = await prisma.movie.create({ data: { id: +filmId, rating: +req.body } })
+            }
+        } catch (error) {
+            res.status(500).json(JSON.stringify(error))
+        }
+    }
+    movie ?
+        res.json({rating: movie.rating}) : res.json({ rating: 0 });
+
+}
